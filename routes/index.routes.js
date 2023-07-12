2133e16e67a575029c59c7efbb09e42a13d43d96
Emilie Luong <emi.luong@gmail.com>
2023-07-12 17:07:41 +0200

    pushing in main

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+<<<<<<< HEAD
   console.log("hello")
+=======
+  console.log('blabla')
+>>>>>>> 54a631f (pushing in main)
   res.render("index");
 });
 
