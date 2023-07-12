cb35485367001fc5cb4c61742284ac73f29f5ef9
Emilie Luong <emi.luong@gmail.com>
2023-07-12 16:38:05 +0200

    trying to push on develop branch

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  console.log("hello")
+  console.log("trying to push on develop")
   res.render("index");
 });
 
