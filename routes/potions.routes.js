9f2b11f76ff52abf7913fa30880db953a0ef53df
Emilie Luong <emi.luong@gmail.com>
2023-07-14 12:55:50 +0200

    all potions and potion page created, linked to routes and databse

--- routes/potions.routes.js
+++ routes/potions.routes.js
@@ -8,22 +8,23 @@ const uploader = require('../config/cloudinary.config.js');
 //GET all potions page
 router.get('/', async (req, res) => {
     try {
-        res.send("all potions here");
+        const allPotions = await Potion.find()
+        res.render("potions-pages/allPotions", {allPotions})
     } catch (error) {
         console.error(error);
     }
 })
 
 //GET ONE potion page
-router.get('/potion/:potionId', async(req,res)=>{
+router.get('/potion/:potionId', async (req, res) => {
     const potionId = req.params.potionId
     try {
         const currentPotion = await Potion.findById(potionId)
-        res.render("potions-pages/potion", {potion : currentPotion})
+        res.render("potions-pages/potion", { potion: currentPotion })
     } catch (error) {
         console.error(error)
     }
-    
+
 })
 
 //GET create page only if you're logged in
@@ -31,21 +32,19 @@ router.get('/create', isLoggedIn, (req, res) => {
     res.render("potions-pages/create")
 })
 
-//POST create page
+//POST create page using CLOUDINARY as a middleware
 router.post('/create', uploader.single("img_url"), async (req, res) => {
     let aPotion
-    
     //checking if user inserting an image, if not, the default one will be display
-    if(req.file){
+    if (req.file) {
         //creating a copy of the request body and assign the image path to the img_url property to match DB
-    aPotion = {...req.body, img_url : req.file.path}
-    }else{
+        aPotion = { ...req.body, img_url: req.file.path }
+    } else {
         aPotion = req.body
     }
-    
     try {
-    const newPotion = await Potion.create(aPotion)
-    res.redirect(`/potions/potion/${newPotion._id}`)
+        const newPotion = await Potion.create(aPotion)
+        res.redirect(`/potions/potion/${newPotion._id}`)
     } catch (error) {
         console.error(error);
     }
