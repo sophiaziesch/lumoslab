b2e0b6c3f298ef9f6628459949fc712309a00d6f
Emilie Luong <emi.luong@gmail.com>
2023-07-17 16:55:17 +0200

    update profile route DONE

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { isLoggedIn } = require('../middleware/middleware');
 const User = require('../models/User.model');
 const router = express.Router();
+const bcrypt = require("bcryptjs")
 
 const uploader = require('../config/cloudinary.config.js');
 
@@ -9,41 +10,76 @@ let loggedIn
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  if(req.session.user){
-loggedIn = true
-  }else{
+  if (req.session.user) {
+    loggedIn = true
+  } else {
     loggedIn = false
   }
-  res.render("index", {user : req.session.user, loggedIn});
+  res.render("index", { user: req.session.user, loggedIn });
 });
 
-router.get('/profile', isLoggedIn, (req, res)=>{
-  res.render("profile", {user : req.session.user, loggedIn})
+router.get('/profile', isLoggedIn, (req, res) => {
+  loggedIn = true
+  res.render("profile", { user: req.session.user, loggedIn })
 })
 
 //GET update user info page
-router.get('/profile/update',isLoggedIn, async (req, res)=>{
-
+router.get('/profile/update', isLoggedIn, async (req, res) => {
+  loggedIn = true
   const currentUser = req.session.user
   try {
-    const updateUser = await User.findOne({username : currentUser.username})
-    res.render('profile-update', {user : updateUser, loggedIn})
+    const updateUser = await User.findOne({ username: currentUser.username })
+    res.render('profile-update', { user: updateUser, loggedIn })
   } catch (error) {
     console.error(error)
   }
 })
 
 //POST update user info page
-router.post('/profile/update',isLoggedIn, uploader.single("img_url"), async (req, res)=>{
+router.post('/profile/update', isLoggedIn, uploader.single("img_url"), async (req, res) => {
+  loggedIn = true
   const currentUser = req.session.user
+  const actualPassword = req.body.password
+  const newPassword = req.body.newPassword
+  const confirmedNewPassword = req.body.confirmedNewPassword
+  const newInfos = {}
+
   try {
-    const updateUser = await User.findOneAndUpdate({username : currentUser.username}, req.body)
+    const findCurrentUser = await User.findOne({ username: currentUser.username })
+    //When the user wants to change his password
+    if (actualPassword || newPassword || confirmedNewPassword) {
+      //checking if the pw typed is same as actual pw
+      if (bcrypt.compareSync(actualPassword, findCurrentUser.passwordHash)) {
+        //if the new pw and the confirmed pw match
+        if (newPassword === confirmedNewPassword) {
+          //generatiing a SALT to use it in "hashSync()" method to encrypt our pw
+          const salt = bcrypt.genSaltSync(13)
+          //encrypting the newpassword and asign it to a new property matching our data
+          newInfos.passwordHash = bcrypt.hashSync(newPassword, salt)
+        } else {
+          res.render('profile-update', { wrongNewPassword: "The new password and the confirmation doesn't match, try again" , loggedIn })
+        }
+      } else {
+        res.render('profile-update', { wrongPassword: "Wrong Password" , loggedIn })
+      }
+    }
+
+    //If an image is uplaoded
+    if(req.file){
+      //then we create a new property with the url of the img on newInfos
+      newInfos.img_url = req.file.path
+    }
+
+    //Update user infos in the database
+    const updateUser = await User.findOneAndUpdate({ username: currentUser.username }, newInfos)
+    res.redirect("/profile")
+
   } catch (error) {
     console.error(error)
   }
 })
 
-router.get("/logout", isLoggedIn, (req, res)=>{
+router.get("/logout", isLoggedIn, (req, res) => {
   delete req.session.user
   res.redirect("/")
 })
