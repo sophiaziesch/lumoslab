70d6268dcb979093c65d1691a6b8630656d0b38e
Sophia Ziesch <s.ziesch@posteo.de>
2023-07-17 15:18:17 +0200

    started styling of signup page

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,19 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("load", () => {
-	let navDropdown = document.getElementById("nav-dropdown");
-	let signUp = document.getElementById("sign-up");
-	let login = document.getElementById("login]");
-	let createPotion = document.getElementById("create-potion");
-	let myProfile = document.getElementById("my-profile");
-
-	if (isLoggedIn) {
-		navDropdown.style.display = "flex";
-		signUp.style.display = "none";
-		login.style.display = "none";
-	} else {
-		navDropdown.style.display = "none";
-		createPotion.style.display = "none";
-		myProfile.style.display = "none";
-	}
-
+	// let navDropdown = document.getElementById("nav-dropdown");
+	// let signUp = document.getElementById("sign-up");
+	// let login = document.getElementById("login]");
+	// let createPotion = document.getElementById("create-potion");
+	// let myProfile = document.getElementById("my-profile");
+	// if (isLoggedIn) {
+	// 	navDropdown.style.display = "flex";
+	// 	signUp.style.display = "none";
+	// 	login.style.display = "none";
+	// } else {
+	// 	navDropdown.style.display = "none";
+	// 	createPotion.style.display = "none";
+	// 	myProfile.style.display = "none";
+	// }
 	// let navDropdown = document.getElementById("nav-dropdown");
 	// if (isLoggedIn) {
 	// 	navDropdown.setAttribute("display", "flex");
