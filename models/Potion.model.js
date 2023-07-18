f4621f32d9596ceab700ef39f42ff5de8f6d7994
Sophia Ziesch <s.ziesch@posteo.de>
2023-07-18 17:24:45 +0200

    potion page styling done

--- models/Potion.model.js
+++ models/Potion.model.js
@@ -1,36 +1,36 @@
 const { Schema, model } = require("mongoose");
 
 const potionSchema = new Schema(
-   {
-      name: {
-         type: String,
-         unique : true,
-         trim: true,
-         required: true,
-      },
-      ingredients: [
-         {
-            type : String,
-            trim: true
-         }
-      ],
-      difficulty: {
-         type: String
-      },
-      characteristics: String,
-      time: Number,
-      effects: String,
-      sideEffects: String,
-      createdBy: String,
-      img_url: {
-         type : String,
-         default: "/images/default_pot.png"
-      }
-   },
-   {
-      // this second object adds extra properties: `createdAt` and `updatedAt`    
-      timestamps: true
-   }
+	{
+		name: {
+			type: String,
+			unique: true,
+			trim: true,
+			required: true,
+		},
+		ingredients: [
+			{
+				type: String,
+				trim: true,
+			},
+		],
+		difficulty: {
+			type: String,
+		},
+		characteristics: String,
+		time: Number,
+		effects: String,
+		sideEffects: String,
+		createdBy: String,
+		img_url: {
+			type: String,
+			default: "/images/default_pot.png",
+		},
+	},
+	{
+		// this second object adds extra properties: `createdAt` and `updatedAt`
+		timestamps: true,
+	}
 );
 
 const Potion = model("Potion", potionSchema);
