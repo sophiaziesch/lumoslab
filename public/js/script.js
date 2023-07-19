9ae976083e41bb22bdc1d8bb7ba8e32c6cd39f5f
Emilie Luong <emi.luong@gmail.com>
2023-07-19 17:23:20 +0200

    ADD INGREDIENT button ok

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,28 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("load", () => {
-	// let navDropdown = document.getElementById("nav-dropdown");
-	// let signUp = document.getElementById("sign-up");
-	// let login = document.getElementById("login]");
-	// let createPotion = document.getElementById("create-potion");
-	// let myProfile = document.getElementById("my-profile");
-	// if (isLoggedIn) {
-	// 	navDropdown.style.display = "flex";
-	// 	signUp.style.display = "none";
-	// 	login.style.display = "none";
-	// } else {
-	// 	navDropdown.style.display = "none";
-	// 	createPotion.style.display = "none";
-	// 	myProfile.style.display = "none";
-	// }
-	// let navDropdown = document.getElementById("nav-dropdown");
-	// if (isLoggedIn) {
-	// 	navDropdown.setAttribute("display", "flex");
-	// } else if (isLoggedOut) {
-	// 	navDropdown.setAttribute("display", "none");
-	// }
+window.addEventListener("load", () => {
+
+	const addIngredientBtn = document.querySelector('.add_ingredient')
+	const ingredients = document.querySelector(".ingredients")
+	const removeIngredientBtn = document.querySelectorAll('.remove_ingredient')
+
+	//Create a new input whenever we click on "Add ingredient button"
+	addIngredientBtn.addEventListener('click', () => {
+		//Creating a new div
+		const newDiv = document.createElement('div')
+		newDiv.className = "oneIngredient"
+		ingredients.appendChild(newDiv)
+
+		//Creating an input in the new Div
+		const newInput = document.createElement('input')
+		newInput.className = "data-input"
+		newInput.name = "ingredients"
+		newDiv.appendChild(newInput)
+		//also creating its own "remove ingredient" button, inside the div
+		const newRemoveBtn = document.createElement('button')
+		newRemoveBtn.className = "remove_ingredient button"
+		newRemoveBtn.textContent = " - "
+		newRemoveBtn.type="button"
+		newDiv.appendChild(newRemoveBtn)
+	})
+
+
 });
